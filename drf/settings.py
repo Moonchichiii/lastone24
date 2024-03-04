1b97980450e4b43b2fbec736fb1d2297bdb1d4e2
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-03-04 11:41:11 +0100

    deployment testing

--- drf/settings.py
+++ drf/settings.py
@@ -43,8 +43,6 @@ CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', cast=Csv())
 
 
 # CSRF and CORS settings
-CSRF_COOKIE_HTTPONLY = True
-CSRF_COOKIE_SECURE = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True  
 
@@ -140,13 +138,14 @@ WSGI_APPLICATION = 'drf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'))}
 
 
 
